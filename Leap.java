373cecfd287e5e5773538b4821d9475c608703e5
Errakolla-Shailaja <shailajayadav354@gmail.com>
2024-06-21 09:36:11 +0530

    Add files via upload

---
+++ Leap.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class Leap
+{
+public static void main(String args[])
+{
+Scanner s=new Scanner(System.in);
+int a;
+a=s.nextInt();
+if((a%4==0)&&(a%100!=0)&&(a%400==0))
+System.out.println("Leap Year");
+else
+System.out.println("Not a Leap Year");
+}
+}
