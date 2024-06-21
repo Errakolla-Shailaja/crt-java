cdcbdb732a05e512f5dbaa52f415652c4cde717c
Errakolla-Shailaja <shailajayadav354@gmail.com>
2024-06-21 09:34:46 +0530

    Add files via upload

---
+++ Div.java
@@ -0,0 +1,12 @@
+import  java.util.*;
+class Div{
+public static void main(String args[]){
+Scanner s=new Scanner(System.in);
+int a,b;
+a=s.nextInt();
+b=s.nextInt();
+float div=a/b;
+System.out.println(div);
+}
+}
+
