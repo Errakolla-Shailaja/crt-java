be48df725fcec094bf47fa4ee75bdac89cb685d7
Errakolla-Shailaja <shailajayadav354@gmail.com>
2024-06-21 09:35:11 +0530

    Add files via upload

---
+++ Even.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class Even
+{
+public static void main(String args[])
+{
+Scanner s=new Scanner(System.in);
+int a;
+a=s.nextInt();
+if(a%2==0)
+System.out.println("Even");
+else
+System.out.println("Odd");
+}
+}
