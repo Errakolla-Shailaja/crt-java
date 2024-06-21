08a56ef34203ce8cd36ac8def424c39ff2cbc167
Errakolla-Shailaja <shailajayadav354@gmail.com>
2024-06-21 09:29:50 +0530

    Add files via upload

---
+++ Addition1.java
@@ -0,0 +1,11 @@
+class Addition1{
+public static void main(String args[])
+{
+int a,b,c;
+a=Integer.parseInt(args[0]);//it converts integer
+b=Integer.parseInt(args[1]);
+c=a+b;
+System.out.println(c);
+}
+} 
+//provide values at runtime while java Addition1 values at execution time becauseof commandline args
