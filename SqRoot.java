e344f9b1eeb9349dd3324d4a6c469e9fc1e9aea1
Errakolla-Shailaja <shailajayadav354@gmail.com>
2024-06-21 09:38:01 +0530

    Add files via upload

---
+++ SqRoot.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class SqRoot{
+public static void main(String args[])
+{
+Scanner s=new Scanner(System.in);
+int a;
+a=s.nextInt();
+double b=Math.sqrt(a);//sqroot means return type is double
+System.out.println(b);
+}
+}
