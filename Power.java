01a3c26b9f86e4a72deb16e2d92e448dda422726
Errakolla-Shailaja <shailajayadav354@gmail.com>
2024-06-21 09:37:03 +0530

    Add files via upload

---
+++ Power.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class Power
+{
+public static void main(String args[])
+{
+Scanner s=new Scanner(System.in);
+a=s.nextInt();
+b=s.nextInt();
+double b1=Math.pow(a,b);
+System.out.println(b1);
+}
+}
